a10b7da86abfdb4000f4ebad8f7ac626fefe39da
gingersmith4 <88351672+gingersmith4@users.noreply.github.com>
2021-08-18 20:47:04 +0000

    finished individual portion

--- lab05Warmup_Ginger.py
+++ lab05Warmup_Ginger.py
@@ -27,7 +27,32 @@ def invert(im):
         green = 255 - green
         blue = 255 - blue
         im.putpixel((x, y), (red, green, blue))
-invert(bear)
+
+#invert(bear)
+#bear.save("tmp_Ginger.png") # create/overwrite tmp_Ginger.png with current image, click on file to view
+#invert(pattern)
+#pattern.save("tmp_pattern.png")
+
+def invert_block( im ):
+  '''only inverts the pixels that are in the upper-right quadrant of the image (so it only inverts 25% of the image) and leaves the others unchanged'''
+
+  #find the dimensions of the image
+  (width, height) = im.size
+  #width/2 to width
+  #0 to height/2
+
+  #loop over the entire image
+  for x in range ( int(width/2), width ):
+    for y in range ( 0, int(height/2) ):
+      #calculate the new pixel values
+      #put
+        (red, green, blue, opacity) = im.getpixel((x, y))
+        red = 255 - red
+        green = 255 - green
+        blue = 255 - blue
+        im.putpixel((x, y), (red, green, blue))
+
+invert_block(bear)
 bear.save("tmp_Ginger.png") # create/overwrite tmp_Ginger.png with current image, click on file to view
-invert(pattern)
+invert_block(pattern)
 pattern.save("tmp_pattern.png")
